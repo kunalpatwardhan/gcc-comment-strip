21da3277bb51375fd1425a5fb46478532dde0663
Kunal Patwardhan <kunal.patwardhan@gmail.com>
2017-01-15 12:05:52 +0530

    Initial Commit
    -- sync method works
    -- filters applied
    -- no gui

---
+++ cmtStrp/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Linq;
+namespace cmtStrp
+{
+	class MainClass
+	{
+		/// &lt;span class="code-SummaryComment">&lt;summary>&lt;/span>
+		/// Executes a shell command synchronously.
+		/// &lt;span class="code-SummaryComment">&lt;/summary>&lt;/span>
+		/// &lt;span class="code-SummaryComment">&lt;param name="command">string command&lt;/param>&lt;/span>
+		/// &lt;span class="code-SummaryComment">&lt;returns>string, as output of the command.&lt;/returns>&lt;/span>
+		public static string ExecuteCommandSync(object command)
+		{
+			string result="";
+			try
+			{
+				// create the ProcessStartInfo using "cmd" as the program to be run,
+				// and "/c " as the parameters.
+				// Incidentally, /c tells cmd that we want it to execute the command that follows,
+				// and then exit.
+				System.Diagnostics.ProcessStartInfo procStartInfo =
+					new System.Diagnostics.ProcessStartInfo("gcc", " -fpreprocessed -dD -E -P " + command);
+
+				// The following commands are needed to redirect the standard output.
+				// This means that it will be redirected to the Process.StandardOutput StreamReader.
+				procStartInfo.RedirectStandardOutput = true;
+				//procStartInfo.RedirectStandardError = true;
+				procStartInfo.UseShellExecute = false;
+				// Do not create the black window.
+				procStartInfo.CreateNoWindow = true;
+				// Now we create a process, assign its ProcessStartInfo and start it
+				System.Diagnostics.Process proc = new System.Diagnostics.Process();
+				proc.StartInfo = procStartInfo;
+				proc.Start();
+				//proc.WaitForExit();
+				// Get the output into a string
+				//proc.WaitForExit();
+				//proc.StandardError.ReadToEnd();
+				result = proc.StandardOutput.ReadToEnd();
+
+				// Display the command output.
+
+				//Console.WriteLine(result);
+			}
+			catch (Exception objException)
+			{
+				// Log the exception
+			}
+		//	string filePath = (string)command;
+
+			return result;
+		}
+
+		/// <span class="code-SummaryComment"><summary></span>
+		/// Execute the command Asynchronously.
+		/// <span class="code-SummaryComment"></summary></span>
+		/// <span class="code-SummaryComment"><param name="command">string command.</param></span>
+		public static void ExecuteCommandAsync(string command)
+		{
+			try
+			{
+				//Asynchronously start the Thread to process the Execute command request.
+				Thread objThread=null;// = new Thread(new ParameterizedThreadStart(ExecuteCommandSync));
+				//Make the thread as background thread.
+				objThread.IsBackground = true;
+				//Set the Priority of the thread.
+				objThread.Priority = ThreadPriority.AboveNormal;
+				//Start the thread.
+				objThread.Start(command);
+			}
+			catch (ThreadStartException objException)
+			{
+				// Log the exception
+			}
+			catch (ThreadAbortException objException)
+			{
+				// Log the exception
+			}
+			catch (Exception objException)
+			{
+				// Log the exception
+			}
+		}
+
+		static void DirSearch(string sDir)
+		{
+			try
+			{
+				foreach (string d in Directory.GetDirectories(sDir))
+				{
+					var filteredFiles = Directory
+						.GetFiles(d, "*.*")
+						.Where(file => file.ToLower().EndsWith("c") || file.ToLower().EndsWith("cpp") 
+							|| file.ToLower().EndsWith("h"))
+						.ToList();
+					foreach (string f in filteredFiles)
+					{
+						
+						string commandStr = "\"" + f + "\""; 
+						Console.WriteLine( d.Substring(d.Length-10,10));
+						if(File.Exists(f))
+						{
+							string result = ExecuteCommandSync(commandStr);
+							if(result != "")
+								File.WriteAllText(f, result);
+						}
+					}
+					DirSearch(d);
+				}
+			}
+			catch (System.Exception excpt)
+			{
+				Console.WriteLine(excpt.Message);
+			}
+		}
+
+		public static void Main (string[] args)
+		{
+			
+			Console.WriteLine ("Let's Begin!");
+
+			DirSearch ("/media/kunal/E6F0D65CF0D63313/Kunal/Mitsbishi/Original CCPU SourceCode2/");
+
+		}
+	}
+}
